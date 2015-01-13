4f6de492dbe1bfa699ca95cb07c9976b70008472
Franco N. Colaizzi <fnc314@gmail.com>
2015-01-12 20:35:50 -0500

    added controller and test files

--- app/app.js
+++ app/app.js
@@ -1,2 +1,15 @@
 'use strict';
 
+// require statements pulling in each module's JS file
+
+
+// array of dependencies
+var dependencyArray = [
+  // Each module name goes here
+
+]
+
+// Declaration of application via name and adding config of routes and config of global urls available (like URIConfig)
+var HairEtcPittsburgh = angular.module('HairEtcPittsburgh', dependencyArray)
+  // .config(require(app/config/route.js))
+  // .constant('UriConfig', require(app/config/uri))
--- app/config/route.js
+++ app/config/route.js
@@ -1,1 +1,33 @@
 'use strict';
+
+module.exports = ['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .otherwise({
+      redirectTo: '/home'
+    })
+    .when('/home', {
+      templateUrl: 'modules/home/home.html',
+      controller: 'HomeController as homeCtrl'
+    })
+    .when('/stylists', {
+      templateUrl: 'modules/stylists/stylists.html',
+      controller: 'StylistController as stylistCtrl'
+    })
+    .when('/offerings', {
+      templateUrl: 'modules/offerings/offerings.html',
+      controller: 'OfferingsController as offeringCtrl'
+    })
+    .when('/contact-us', {
+      templateUrl: 'modules/contact-us/contact-us.html',
+      controller: 'ContactUsController as contactUsCtrl'
+    })
+    .when('/photos', {
+      templateUrl: 'modules/photos/photos.html',
+      controller: 'PhotosController as photoCtrl',
+    })
+    .when('/about-us', {
+      templateUrl: 'modules/about-us/about-us.html',
+      controller: 'AboutUsController as aboutUsCtrl'
+    })
+
+}];
